3548214602cc07e06a721f81e1708ded04b0f99f
corinB <gkdisrha2020@gmail.com>
2023-11-07 14:41:07 +0900

    리엑트 시작

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import './page/landing'
+
+function App() {
+  let post ='백종현';
+  
+
+  return (
+    <div className="App">
+      <div className = "abc">
+      <h4>hhh</h4>
+      </div>
+      <h4>{post}</h4>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/page/landing.js
@@ -0,0 +1,18 @@
+import logo from '../logo.svg';
+import './landing.css';
+
+
+export const Landing = () =>{
+    let post ='백종현입니다. 안녕하세요';
+    
+  
+    return (
+      <div className="App">
+        <div className = "abc">
+        <h4>hhh</h4>
+        </div>
+        <h4>{post}</h4>
+      </div>
+    );
+  };
+
